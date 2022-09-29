ce7f5290a4726f7cab420f5fd44c638139342865
Cyd Tseng <63691277+cydtseng@users.noreply.github.com>
2022-09-29 23:30:42 +0800

    Updated old print calls and var naming.

--- zip_crack/zip_crack.py
+++ zip_crack/zip_crack.py
@@ -5,7 +5,7 @@ from threading import Thread
 def crack(the_file, password):
     try:
         the_file.extractall(pwd=password)
-        print '[+] Found password ' + password + '\n' 
+        print('[+] Found password ' + password + '\n')
     except:
         pass
 
@@ -19,14 +19,14 @@ def main():
     (options, args) = parse.parse_args()
 
     if (options.zname == None || options.dname == None):
-        print parser.usage
+        print(parser.usage)
         exit(0)
     else:
         zname = options.zname
         dname = options.dname
         file_to_crack = zf.ZipFile(zname)
-        dictFile = open(dname)
-        for line in dictFile.readlines():
+        dict_file = open(dname)
+        for line in dict_file.readlines():
             password = line.strip('\n')
             # start a new thread to crack file with diff password
             t = Thread(target=crack, args=(file_to_crack, password))
