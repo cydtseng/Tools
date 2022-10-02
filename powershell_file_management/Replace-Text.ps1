85d0b4b8b3493c857d9a7fc957a0553863468395
Cyd Tseng <63691277+cydtseng@users.noreply.github.com>
2022-10-02 15:39:02 +0800

    Added Text Replacement script

---
+++ powershell_file_management/Replace-Text.ps1
@@ -0,0 +1,12 @@
+﻿$file = Read-Host "File Path"
+$tempFile = [System.IO.Path]::GetTempFileName()
+
+$targetText = "target text"
+$replacementText = "replacement text"
+
+Get-Content $file | 
+    Foreach-Object {$_ -creplace $targetText, $replacementText} | 
+    Add-Content $tempFile
+
+Remove-Item $file
+Move-Item $tempFile $file
